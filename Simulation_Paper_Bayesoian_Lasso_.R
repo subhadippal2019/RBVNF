535edee92cbea714c68513861bb4624c46057c16
subhadippal2019 <50275156+subhadippal2019@users.noreply.github.com>
2024-12-01 23:50:22 +0400

    sd

--- Simulation_Paper_Bayesoian_Lasso_.R
+++ Simulation_Paper_Bayesoian_Lasso_.R
@@ -12,7 +12,7 @@
 
 n=500  # NUmber of the samples
 p=20    # NUmber of the regression covariates
-d=3    # Number of direcions in the direcional data
+d=2   # Number of direcions in the direcional data
 
 Num_of_nonzero_beta= round(p*.10)
 Min_Non_Zero_beta= 1
@@ -27,7 +27,7 @@ tau_square=1000
 #######################################################################################################
 #######################################################################################################
 
-for(SimNum in 1:3){
+for(SimNum in 51:55){
 
 
   #data_lst = Data_generator_vnf_reg(n=n, p=p, d=d, concentration_factor = 1, beta_factor = 10)
@@ -58,14 +58,14 @@ for(SimNum in 1:3){
 
   #lst=MCMC_Dir_regression_sampler_V1(Y=data_lst$Y, X=data_lst$X, prior=NULL, beta_init = NULL, MCSamplerSize =5500)
 
-  MC_SIM_Reg_Dir_Data<-list(MC_lst=lst, Sim_Data_lst=data_lst)
+  MC_SIM_Reg_Dir_Data<-list(MC_lst=lst_BLASSO_Beta_MCMC, Sim_Data_lst=data_lst)
 
 
   save(MC_SIM_Reg_Dir_Data, file=paste0("/Users/subhadippal/Desktop/Lasso_Simulation_RBVNF/MC_SIM_BLASSO_Reg_Dir_Data_d_eq_",d, "_SimNUmber_",SimNum, ".RData" ))
   #save(MC_SIM_Reg_Dir_Data, file=paste0("G:\\My Drive\\MyRPackages\\MC_SIM_Reg_Dir_Data\\MC_SIM_Reg_Dir_Data_n_750_d_eq_",d, "_SimNUmber_",SimNum, ".RData" ))
 
   print(paste0("sim Number", SimNum, "completed"))
-  rm(list=c("MC_SIM_Reg_Dir_Data","lst" , "data_lst" ))
+  rm(list=c("MC_SIM_Reg_Dir_Data","lst_BLASSO_Beta_MCMC" , "data_lst" ))
 }
 
 
