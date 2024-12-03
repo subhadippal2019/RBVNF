377d9edabc6de2997c61240b6bc9e695a63a87a2
subhadippal2019 <50275156+subhadippal2019@users.noreply.github.com>
2024-12-03 18:17:14 +0400

    sd

--- Box_PLlot_all_Coef.R
+++ Box_PLlot_all_Coef.R
@@ -10,19 +10,20 @@
 #Beta_est_Post_median<-apply(Mc_Beta_burnin, MARGIN = c(2,3), FUN = function(xx){median(xx)})
 
 location="/Users/subhadippal/Desktop/Lasso_Simulation_RBVNF/"
- fileName= paste0("MC_SIM_BLASSO_Reg_Dir_Data_d_eq_",2,"_SimNUmber_",30,".RData")
+data_id=35
+ fileName= paste0("MC_SIM_BLASSO_Reg_Dir_Data_d_eq_",2,"_SimNUmber_",data_id,".RData")
       file_with_path= paste0(location,fileName)
       assign('Mc_obj_lso', get( load(file=file_with_path ) ))
 
-plot_beta_p_20_d_2(Mc_obj_lso)
-
+plt<-plot_beta_p_20_d_2(Mc_obj_lso, PlotType = 'vio', zero_marking_linetype=6, zero_marking_linewidth=.5)
+ggsave(plt, file=paste0(location,paste0("MC_SIM_BLASSO_Reg_Dir_Data_d_eq_",2,"_SimNUmber_",data_id,"_beta_plot.pdf")), width = 15, height = 8)
 
 # library
 library(ggplot2)
 
 # create data
 
-plot_beta_p_20_d_2<-function(Mc_obj=Mc_obj_lso, burnIN=1000){
+plot_beta_p_20_d_2<-function(Mc_obj=Mc_obj_lso, burnIN=1000, PlotType="vio", zero_marking_linetype=6, zero_marking_linewidth=.8){
 
 
   #location="/Users/subhadippal/Desktop/Lasso_Simulation_RBVNF/"
@@ -31,6 +32,13 @@ plot_beta_p_20_d_2<-function(Mc_obj=Mc_obj_lso, burnIN=1000){
   #assign('Mc_obj_lso', get( load(file=file_with_path ) ))
 
   #plot_beta_p_20_d_2(Mc_obj_lso)
+
+  data_summary <- function(x) {
+    m <- median(x)
+    ymin <- as.numeric(quantile(x,.025))#m-sd(x)
+    ymax <- as.numeric(quantile(x, 0.975))#m+sd(x)
+    return(c(y=m,ymin=ymin,ymax=ymax))
+  }
 
   Mc_Beta_burnin<-((Mc_obj$MC_lst$MC$Mc_Beta[-(1:burnIN), , ]))
  mc_samples= Mc_Beta_burnin
@@ -56,14 +64,33 @@ my_xlab <- paste0("c(", ((paste("expression(",(beta_names),")",collapse=","))), 
 
 
 p<-ggplot(data, aes(x=Names, y=Value,fill=Names ))
-p<-p+  with_shadow(geom_boxplot(varwidth = TRUE,
-                                alpha=0.85,
+if(PlotType=='vio'){
+  p<-p+with_shadow(geom_hline(yintercept=0, col="white",linewidth=zero_marking_linewidth, linetype=zero_marking_linetype ),  sigma = 1,
+                   x_offset = 0,
+                   y_offset = 0,
+                   colour = "black" )
+  p<-p+  with_shadow(geom_violin(alpha=0.9,
+                                  linewidth = .05, scale = 'width'),
+                     sigma = 3,
+                     x_offset = 0,
+                     y_offset = 0,
+                     colour = "black" )
+  #p<-p+stat_summary(fun.data=mean_sdl, mult=1, geom="pointrange", color="red")
+ p<- p + with_shadow(stat_summary(fun.data=data_summary, size=.01, col="black"),  sigma = 2,
+                     x_offset = 0,
+                     y_offset = 0,
+                     colour = "black" )
+
+}
+if(PlotType!='vio'){
+p<-p+  with_shadow(geom_boxplot(alpha=0.85,
                                 linewidth = .15,
                                 outlier.size = .05, outlier.colour = "black"),
                                 sigma = 2,
                                 x_offset = 0,
                                 y_offset = 0,
                                 colour = "black" )
+}
 
   p<-p+theme(legend.position="none") +scale_x_discrete(labels=eval(parse(text=my_xlab)))
 p<-p+theme(panel.grid = element_line(color = "white",
@@ -74,6 +101,15 @@ p<-p+scale_fill_manual(values=c(replicate(4, "darkolivegreen2"), replicate(36, "
 p<-p+xlab("Regression Coefficients")+ ylab(" ")+ ggtitle(" ")
 return(p)
 }
+
+
+
+
+
+
+
+
+
 
 
 ffff_delete-function(lst,
@@ -165,3 +201,17 @@ ffff_delete-function(lst,
   }
   return(p)
 }
+
+
+
+
+
+#old_theme <- theme_update(
+ # plot.background = element_rect(fill = "lightblue3", colour = NA),
+#  panel.background = element_rect(fill = "lightblue", colour = NA),
+#  axis.text = element_text(colour = "linen"),
+#  axis.title = element_text(colour = "linen")
+#)
+
+#plt+theme_set(old_theme)
+
